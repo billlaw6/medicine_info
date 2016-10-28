31074e9075901528faadb01dba77f96ed908dd93
bill_law6 <liubin6@gmail.com>
2016-10-28 15:49:46 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Login Please!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
